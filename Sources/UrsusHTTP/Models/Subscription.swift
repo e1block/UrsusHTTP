114c0dbb656f03972217d2cc98d28cc34bb82218
e1block <omeed@assembly.capital>
2023-04-18 15:33:25 -0500

    Add Subscription model

---
+++ Sources/UrsusHTTP/Models/Subscription.swift
@@ -0,0 +1,15 @@
+//
+//  Subscription.swift
+//  
+//
+//  Created by ~hosryc-matbel on 4/18/23.
+//
+
+import Foundation
+
+struct Subscription {
+    var id: Int
+    let ship: Ship
+    let path: String
+    let app: String
+}
